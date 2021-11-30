768dc54387c15d3d090a42784f767c2d5d1fcdc6
priscila monteiro <pririmonteiro@me.com>
2021-11-30 13:07:26 -0800

    get per id working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,93 @@ const express = require('express')
 const Post = require('./posts-model')
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+      });
+    });
+})
+
+// | 2   | GET    | /api/posts/:id          | Returns **the post object with the specified id**    
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post with the specified ID does not exist'})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'The post information could not be retrieved',
+      });
+    });
+})
+
+// | 3   | POST   | /api/posts              | Creates a post using the information sent inside the request body and returns **the newly created post object**  
+
+router.post('/', (req, res) => {
+  console.log('get is up')
+  res.json('endpoint working')
+})
+
+// | 4   | PUT    | /api/posts/:id          | Updates the post with the specified id using data from the request body and **returns the modified document**, not the original |
+
+router.put('/', (req, res) => {
+  console.log('get is up')
+  res.json('endpoint working')
+})
+
+// | 5   | DELETE | /api/posts/:id          | Removes the post with the specified id and returns the **deleted post object**             
+
+router.delete('/', (req, res) => {
+  console.log('get is up')
+  res.json('endpoint working')
+})
+
+// | 6   | GET    | /api/posts/:id/comments | Returns an **array of all the comment objects** associated with the post with the specified id                                  |
+
+
+// router.get('/', (req, res) => {
+//   console.log('get is up')
+//   res.json('endpoint working')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
